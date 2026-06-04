e84231d166e497cc1d5c75493f92e2573f156f70
chauhannishith <chauhannishith94@gmail.com>
2026-06-04 17:47:07 +0530

    feat: export fetch adapters and optional peer dependencies

---
+++ src/adapters/index.ts
@@ -0,0 +1,3 @@
+export { BufferPacingLink } from './apollo-link'
+export { createBufferedFetch, type BufferedFetchOptions } from './fetch'
+export { createGraphqlRequestFetch } from './graphql-request'
